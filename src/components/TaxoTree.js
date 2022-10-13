3c899b3b9d6097d4fe002d61a8f0caa85a8f9b6b
Alex Molto <alex.molto@geomatico.es>
2022-10-13 19:47:41 +0200

    refactor

--- src/components/TaxoTree.js
+++ src/components/TaxoTree.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import useDictionaries from '../hooks/useDictionaries';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
@@ -40,35 +40,34 @@ const listItemTextStyle = {
   color: theme => lighten(theme.palette.primary.main, 0.15),
 };
 
+// TODO numero maximo de niveles. Esto molaria sacarlo del config, pero están mezclados los 7 niveles y los 3 filtros.
+const MAX_LEVELS = 7;
+
 const TaxoTree = () => {
   const dictionaries = useDictionaries();
-
   const [breadCrumbs, setBreadCrumbs] = useState([1]);
-  const [selectedLevelIndex, setSelectedLevelIndex] = useState(1); // guarda el indice del nivel en el que estamos
 
-  // TODO esto es solo para desarrollo, borrar al acabar
-  useEffect(() => {
-    console.log('Level name:', getLevelName(selectedLevelIndex), '---', 'Index:', selectedLevelIndex);
-    console.log('breadCrumbs', breadCrumbs, dictionaries[getLevelName(selectedLevelIndex - 1)].find(el => el.id === breadCrumbs[breadCrumbs.length - 1]));
-  }, [selectedLevelIndex, breadCrumbs]);
+  const getName =(id)=> {
+    const x = dictionaries[getLevelName(breadCrumbs.length - 1)].find(el => el.id === id);
+    return x ? x.name : '';
+  };
 
   const getLevelName = (level) => {
     return Object.keys(DATA_PROPS)[level];
   };
 
-  const hasSubLevelItems = (element) => {
-    return dictionaries[getLevelName(selectedLevelIndex + 1)]
-      .filter(x => x[getLevelName(selectedLevelIndex) + '_id'] === element.id)
+  const hasSubLevels = (element) => {
+    return dictionaries[getLevelName(breadCrumbs.length + 1)]
+      .filter(x => x[getLevelName(breadCrumbs.length) + '_id'] === element.id)
       .filter(y => y.name !== '')
       .length > 0;
   };
 
   const handleOnNextLevelClick = (element) => {
     // corto la navegacion en subespecies (nivel 7) o si no hay resultados por debajo de este nivel
-    if (selectedLevelIndex < 7 && hasSubLevelItems(element)) {
-      const newBreadcrumb = Array.from(breadCrumbs.concat([element.id]));
+    if (breadCrumbs.length < MAX_LEVELS && hasSubLevels(element)) {
+      const newBreadcrumb = Array.from(breadCrumbs).concat([element.id]);
       setBreadCrumbs(newBreadcrumb);
-      setSelectedLevelIndex(selectedLevelIndex + 1);
     }
   };
 
@@ -76,19 +75,18 @@ const TaxoTree = () => {
     if (breadCrumbs.length > 1) {
       const newBreadcrumb = Array.from(breadCrumbs).splice(0, breadCrumbs.length - 1);
       setBreadCrumbs(newBreadcrumb);
-      setSelectedLevelIndex(selectedLevelIndex - 1);
     }
   };
 
-  const selectedTaxo = dictionaries[getLevelName(selectedLevelIndex)]
-    .filter(el => el[getLevelName(selectedLevelIndex - 1) + '_id'] === breadCrumbs[breadCrumbs.length - 1]);
+  const selectedTaxo = dictionaries[getLevelName(breadCrumbs.length)]
+    .filter(el => el[getLevelName(breadCrumbs.length - 1) + '_id'] === breadCrumbs[breadCrumbs.length - 1]);
 
   return <>
     <Box sx={contentTaxoStyle} onClick={handleOnPreviousLevelClick}>
       {
         breadCrumbs.length > 1 && <KeyboardReturnIcon sx={iconTaxoStyle}/>
       }
-      <Typography sx={labelTaxoStyle}>{getLevelName(selectedLevelIndex - 1)}</Typography>
+      <Typography sx={labelTaxoStyle}>{getName(breadCrumbs[breadCrumbs.length - 1])}</Typography>
     </Box>
     <List dense sx={{ml: 2}}>
       {
