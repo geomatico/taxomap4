db6c4b30db5f74cc6945f97da6ec0c33f5baa6c7
Alexandra <39796041+sinsit1@users.noreply.github.com>
2023-07-27 11:32:07 +0200

    Añade BBOX a grafica (#24)
    Co-authored-by: Alex Molto <alex.molto@geomatico.es>

--- src/components/GraphicByLegend.tsx
+++ src/components/GraphicByLegend.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {FC} from 'react';
 import PieChart from './PieChart';
 import useCount from '../hooks/useCount';
-import {BASIS_OF_RECORD_LEGEND, INSTITUTION_LEGEND, TAXONOMIC_LEVELS} from '../config';
+import {BASIS_OF_RECORD_LEGEND, INSTITUTION_LEGEND} from '../config';
 import useArrowData from '../hooks/useArrowData';
 import useDictionaries from '../hooks/useDictionaries';
+import {BBOX, LegendItem, SymbolizeBy, TaxomapData, Taxon, YearRange} from '../commonTypes';
 
-const calculatePercentage = (partialValue, total) => partialValue * 100 / total;
+const calculatePercentage = (partialValue: number, total: number) => partialValue * 100 / total;
 
-const GraphicByLegend = ({institutionFilter, basisOfRecordFilter, yearFilter, taxonFilter, symbolizeBy}) => {
-  const data = useArrowData();
+export interface GraphicByLegendProps {
+  taxonFilter: Taxon,
+  symbolizeBy: SymbolizeBy,
+  institutionFilter?: number,
+  basisOfRecordFilter?: number,
+  yearFilter?: YearRange,
+  BBOX?: BBOX
+}
+
+const GraphicByLegend:FC<GraphicByLegendProps> = ({institutionFilter, basisOfRecordFilter, yearFilter, taxonFilter, symbolizeBy, BBOX}) => {
+  const data: TaxomapData | undefined = useArrowData();
   const dictionaries = useDictionaries();
 
-  const totals = useCount({data, dictionaries, institutionFilter, basisOfRecordFilter, yearFilter, selectedTaxon: taxonFilter, symbolizeBy});
+  const totals = useCount({data, dictionaries, institutionFilter, basisOfRecordFilter, yearFilter, selectedTaxon: taxonFilter, symbolizeBy, BBOX});
 
   const sumTotalresults = Object.keys(totals).length && Object.values(totals).reduce((a, b) => a + b);
 
   const formattedForChart = Object.keys(totals).length && Object.entries(totals).map(([key, value]) => {
-    let elementConf = {};
+    let elementConf: LegendItem |  undefined = {} as LegendItem;
+
     if (symbolizeBy === 'institutioncode') {
       elementConf = INSTITUTION_LEGEND.find(el => el.id === parseInt(key));
     } else if (symbolizeBy === 'basisofrecord') {
       elementConf = BASIS_OF_RECORD_LEGEND.find(el => el.id === parseInt(key));
     }
+    if(!elementConf) return undefined;
 
     return {
       color: elementConf.color,
@@ -30,20 +41,9 @@ const GraphicByLegend = ({institutionFilter, basisOfRecordFilter, yearFilter, ta
       id: elementConf.id,
       percentage: calculatePercentage(value, sumTotalresults)
     };
-  });
+  }).filter(el => el !== undefined);
 
   return <> {formattedForChart && <PieChart data={formattedForChart}/>}</>;
-};
-
-GraphicByLegend.propTypes = {
-  institutionFilter: PropTypes.number,
-  basisOfRecordFilter: PropTypes.number,
-  yearFilter: PropTypes.arrayOf(PropTypes.number),
-  symbolizeBy: PropTypes.string,
-  taxonFilter: PropTypes.shape({
-    level: PropTypes.oneOf(TAXONOMIC_LEVELS).isRequired,
-    id: PropTypes.number.isRequired
-  })
 };
 
 GraphicByLegend.defaultProps = {};
