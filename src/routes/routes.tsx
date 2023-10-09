8a159d0c0b0ca454a1f925b514a20a061cc2cc7f
Oscar Fonts <oscar.fonts@geomati.co>
2023-10-09 13:42:13 +0200

    TAX-41 | Llegir idioma per query param

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,29 +1,14 @@
 import React, {FC} from 'react';
-import {HashRouter, Navigate, Outlet, Route, Routes, useParams} from 'react-router-dom';
-import i18n from 'i18next';
+import {HashRouter, Navigate, Route, Routes} from 'react-router-dom';
 import MapView from '../views/Map';
-
-const LangSetter: FC = () => {
-  const {lang} = useParams();
-  if (i18n.resolvedLanguage !== lang) {
-    i18n.changeLanguage(lang);
-  }
-  return <Outlet/>;
-};
 
 const AppRoutes: FC = () =>
   <HashRouter>
     <Routes>
-      <Route path=":lang" element={<LangSetter/>}>
-        <Route path="" element={<Navigate to="map"/>}/>
-        <Route path="map" element={<MapView/>}/>
-        <Route path="map/:level/:id" element={<MapView/>}/>
-        {/*
-         <Route exact path="detail" element={<Layout mainContent={<MapView/>} miniSidePanelSelectedActionId='detail'/>}/>
-        */}
-        <Route path="*" element={<>404</>}/>
-      </Route>
-      <Route path="*" element={<Navigate to={i18n.resolvedLanguage}/>}/>
+      <Route path="" element={<Navigate to="map"/>}/>
+      <Route path="map" element={<MapView/>}/>
+      <Route path="map/:level/:id" element={<MapView/>}/>
+      <Route path="*" element={<>404</>}/>
     </Routes>
   </HashRouter>;
 
