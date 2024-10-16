410c26cd142f4ddca2d3bbec26bffeb74d813df6
Víctor González <1035219+victorzinho@users.noreply.github.com>
2024-10-16 17:26:08 +0200

    TAX-69 | Configure staging deployment

---
+++ devops/docker/backend/scripts/start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+
+if [ -z "${GUNICORN_LOG_LEVEL}" ]; then
+  GUNICORN_LOG_LEVEL="info"
+fi
+if [ -z "${GUNICORN_N_WORKERS}" ]; then
+  GUNICORN_N_WORKERS=2
+fi
+if [ -z "${GUNICORN_N_THREADS}" ]; then
+  GUNICORN_N_THREADS=2
+fi
+
+gunicorn --workers=${GUNICORN_N_WORKERS} --threads=${GUNICORN_N_THREADS} ${GUNICORN_EXTRA_OPTS} --capture-output --log-level "${GUNICORN_LOG_LEVEL}" project.wsgi:application -b 0.0.0.0:80
