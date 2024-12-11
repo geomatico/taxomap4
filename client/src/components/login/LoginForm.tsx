4d84e511d9900a27d1c7e3a804290b7e9adabcc1
martitasea <marta.pulido.asin@gmail.com>
2024-12-11 15:59:35 +0100

    Estiliza login

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -6,7 +6,10 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import LockIcon from '@mui/icons-material/Lock';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import {FormHelperText} from '@mui/material';
+import {Avatar, FormHelperText, Grid, Paper} from '@mui/material';
+import {SxProps} from '@mui/system';
+import {useTranslation} from 'react-i18next';
+import Stack from '@mui/material/Stack';
 
 const errorText = {
   color: 'error.main',
@@ -14,9 +17,8 @@ const errorText = {
   ml: 2
 };
 const loginButton = {
-  height: '48px',
-  mt: 3,
-  mb: 2
+  //height: '48px',
+  mt: 1,
 };
 
 export type LoginFormProps = {
@@ -25,6 +27,9 @@ export type LoginFormProps = {
 }
 
 const LoginForm: FC<LoginFormProps> = ({error, onLogin}) => {
+
+  const {t} = useTranslation();
+
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
@@ -33,48 +38,63 @@ const LoginForm: FC<LoginFormProps> = ({error, onLogin}) => {
     onLogin(email, password);
   };
 
-  return <>
-    <Box component="form" onSubmit={handleLoginClick} noValidate sx={{mt: 12, margin: 'auto', maxWidth:'40em'}}>
-      <TextField
-        type="text"
-        margin="normal"
-        required
-        fullWidth
-        id="email"
-        name="email"
-        autoComplete="email"
-        autoFocus
-        placeholder="Email"
-        onChange={event => setEmail(event.target.value)}
-        value={email}
-        InputProps={{startAdornment: <AccountCircleIcon sx={{marginRight: 1}}/>}}
-      />
-      <TextField
-        margin="normal"
-        required
-        fullWidth
-        name="password"
-        type="password"
-        id="password"
-        autoComplete="current-password"
-        placeholder="Password"
-        onChange={event => setPassword(event.target.value)}
-        value={password}
-        InputProps={{startAdornment: <LockIcon sx={{marginRight: 1}}/>}}
-      />
-      <Typography variant="body2" sx={errorText}>
-        { error && <FormHelperText>{error}</FormHelperText> }
-      </Typography>
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        sx={loginButton}
-      >
-        ENTRAR
-      </Button>
-    </Box>
-  </>;
+  const containerSx: SxProps = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '30vw',
+    gap: 2
+  };
+  
+  return  <Grid container component='main' sx={{height: '100vh'}}>
+    <Grid item xs={false} sm={4} md={5} component={Paper} elevation={6} square
+      sx={{bgcolor: 'common.black', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <img src='./images/logos/logo.png' alt='Bioexplora' width='500px'/>
+    </Grid>
+    <Grid item xs={12} sm={8} md={7} sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <Box component="form" onSubmit={handleLoginClick} noValidate sx={containerSx}>
+        <Stack>
+          <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+            <LockIcon/>
+          </Avatar>
+          <Typography variant='overline' sx={{fontSize: 14}}>{t('login.enter')}</Typography>  
+        </Stack>
+          
+        <TextField
+          type="text"
+          required
+          fullWidth
+          id="email"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          placeholder="Email"
+          onChange={event => setEmail(event.target.value)}
+          value={email}
+          InputProps={{startAdornment: <AccountCircleIcon sx={{marginRight: 1}}/>}}
+        />
+        <TextField
+          required
+          fullWidth
+          name="password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          placeholder="Password"
+          onChange={event => setPassword(event.target.value)}
+          value={password}
+          InputProps={{startAdornment: <LockIcon sx={{marginRight: 1}}/>}}
+        />
+        <Typography variant="body2" sx={errorText}>
+          { error && <FormHelperText>{error}</FormHelperText> }
+        </Typography>
+        <Button type="submit" fullWidth color='secondary' variant="contained" sx={loginButton}>
+          {t('login.enter')}
+        </Button>
+      </Box>
+    </Grid>
+  </Grid>;
+  
 };
 
 export default LoginForm;
