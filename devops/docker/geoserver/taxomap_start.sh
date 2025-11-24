4a64fe258abd46af022183a6056803f5bdd2ac6d
Oscar Fonts <oscar.fonts@geomatico.es>
2025-11-24 10:18:38 +0100

    Fix GeoServer password generation

--- devops/docker/geoserver/taxomap_start.sh
+++ devops/docker/geoserver/taxomap_start.sh
@@ -3,7 +3,7 @@
 if [ -n "${GEOSERVER_ADMIN_PASSWORD}" ];then
   echo "GEOSERVER_ADMIN_PASSWORD has been provided, using it"
   USERS_XML=$GEOSERVER_DATA_DIR/security/usergroup/default/users.xml
-  sed -i 's/<user .*admin.*/<user enabled="true" name="admin" password="'"$GEOSERVER_ADMIN_PASSWORD"'"\/>/g' $USERS_XML
+  sed -i 's/<user .*admin.*/<user enabled="true" name="admin" password="'"plain:$GEOSERVER_ADMIN_PASSWORD"'"\/>/g' $USERS_XML
 fi
 
 chmod +x /usr/local/bin/start.sh
